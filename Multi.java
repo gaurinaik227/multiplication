0f907c0a389d4b20c374e1785ae06d4510c000e4
gaurinaik227 <naik.gauri283@gmail.com>
2022-04-28 12:16:08 +0530

    Add files via upload

---
+++ Multi.java
@@ -0,0 +1,10 @@
+public class Multi {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, m;
+      m = num1 * num2;
+
+      System.out.println("Multiplication of these numbers: "+m);
+   }
+}
